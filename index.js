33de1a07905195472717fb34589f1bad112d12bd
audwo <kmjstj3@naver.com>
2023-11-24 00:50:36 +0900

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const s3 = new S3Client();
 //람다는 3개의 매개변수를 제공하고 이 함수를 호출해준다.
 exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name;
-  const Key = decodeURIComponent(event.Records[0].s3.object.key); //original/리버풀.png
+  const Key = event.Records[0].s3.object.key; //original/리버풀.png
   const filename = Key.split("/").at(-1);
   const ext = Key.split(".").at(-1).toLowerCase();
   const requiredFormat = ext === "jpg" ? "jpeg" : ext;
