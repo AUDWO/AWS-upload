ed4cee67adb9a2a5334b4f7dfc5c83d85c449189
audwo <kmjstj3@naver.com>
2023-11-23 21:17:56 +0900

    update

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const {
   S3Client,
   GetObjectCommand,
   PutObjectCommand,
-} = require("@aws-sdk/client");
+} = require("@aws-sdk/client-s3");
 
 //함수가 aws 람다에서 돌아가기 때문에 스크릿키랑 아이디를 자동으로 넣어준다 = > 아무것도 넣어줄 필요 x
 const s3 = new S3Client();
 
 //람다는 3개의 매개변수를 제공해서 이 함수를 호출해준다.
 exports.handler = async (event, context, callback) => {
-  const Bucket = event.Record[0].s3.bucket.name;
-  const Key = decodeURIComponent(event.Record[0].s3.object.key); //original/리버풀.png
+  const Bucket = event.Records[0].s3.bucket.name;
+  const Key = decodeURIComponent(event.Records[0].s3.object.key); //original/리버풀.png
   const filename = Key.split("/").at(-1);
   const ext = Key.split(".").at(-1).toLowerCase();
   const requiredFormat = ext === "jpg" ? "jpeg" : ext;
   console.log("name", filename, "ext", ext);
   try {
     const s3Object = await s3.send(new GetObjectCommand({ Bucket, Key }));
-    const buffer = [];
-    for await (const data of GetObjectCommand.Body) {
-      buffer.push(data);
+    const buffers = [];
+    for await (const data of getObject.Body) {
+      buffers.push(data);
     }
-    const imageBuffer = Buffer.concat(buffer);
+    const imageBuffer = Buffer.concat(buffers);
 
     console.log("original", s3Object);
     const resizedImage = await sharp(imageBuffer)
